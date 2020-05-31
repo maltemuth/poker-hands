4309a035ed83cf11f3b15ce20c9bc35fcaa197bf
Malte Muth <malte.muth+git-commit@googlemail.com>
2020-05-31 22:47:25 +0200

    refactor(hasEqualValue): refactor method

--- src/model/hand/hasEqualValue.ts
+++ src/model/hand/hasEqualValue.ts
@@ -1,7 +1,22 @@
 import { Hand } from "./Hand";
-import isBetterThan from "./isBetterThan";
+import valueToString from "../card/valueToString";
+import { Card } from "../card/Card";
 
-const hasEqualValue = (a: Hand<any>, b: Hand<any>): boolean =>
-  !isBetterThan(a, b) && !isBetterThan(b, a);
+const serializeValues = (cards: Card[]): string =>
+  cards
+    .map(({ value }) => value)
+    .sort((x, y) => x - y)
+    .map(valueToString)
+    .join("");
+
+const hasEqualValue = (a: Hand<any>, b: Hand<any>): boolean => {
+  if (a.type !== b.type) return false;
+  if (a.value() !== b.value()) return false;
+  if (a.subvalue() !== b.subvalue()) return false;
+  if (serializeValues(a.cards()) !== serializeValues(b.cards())) return false;
+  if (serializeValues(a.kickers()) !== serializeValues(b.kickers()))
+    return false;
+  return true;
+};
 
 export default hasEqualValue;
