e869ccca04f90627630dff777b3eb65d89541caf
Malte Muth <malte.muth+git-commit@googlemail.com>
2020-06-01 11:12:58 +0200

    feat(index): add barrel file for exports

---
+++ index.ts
@@ -0,0 +1,6 @@
+import getBestHand from "./src/model/hand/getBestHand";
+import hasEqualValue from "./src/model/hand/hasEqualValue";
+import isBetterThan from "./src/model/hand/isBetterThan";
+import odds from "./src/model/odds/odds";
+
+export { getBestHand, hasEqualValue, isBetterThan, odds };
